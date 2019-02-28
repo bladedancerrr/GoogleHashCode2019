f50f13d5ab7d0516fe95189b0ad09d88d59801c4
Lexon <lexonli@hotmail.com>
2019-03-01 07:44:33 +1100

    slide stuff

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from init import getPhotoList
 from createSlideshow import createSlideshow
 from createOutput import createOutput
 
+
 def main():
 	photoList = getPhotoList('c')
 	slideshow = createSlideshow(photoList)
+	slideshow.sort(key=lambda x: x.tagNumber, reverse=True)
 	createOutput(slideshow)
 
 if __name__ == '__main__':
