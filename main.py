b20b031a0e8661b660b36c6d7cc604add2bba675
yaoshi <yaos5@student.unimelb.edu.au>
2019-03-01 08:08:39 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from init import getPhotoList
 from createSlideshow import createSlideshow
 from createOutput import createOutput
+from 
 
 def main():
-	photoList = getPhotoList('c')
-	slideshow = createSlideshow(photoList)
-	createOutput(slideshow)
 
-if __name__ == '__main__':
-	main()
+
+
+
+    # read input
+    # Create photos
+    photoList = getPhotoList('c')
+
+    # create slides
+    slides = convertToSlides(photoList)
+
+    # sorted the slides according to their tag numbers create size classes
+    sizeClasses = makeSizeClass(slides)
+
+
+    slideShow = []
+    for sizeClass in sizeClasses:
+    # for each size class
+        # build dis matrix
+        disMatrix = buildMatrix(sizeClass)
+        # iterate through size classes, to find their average /3 * 2, and find row and col, append to slide show, # delete row and column, # update matrix and repeat until we can't anymore
+        miniSlideShow = createSlideshow(disMatrix)
+        slideShow.append(miniSlideShow)
+    # create output
+    createOutput(slideShow)
+
+# if __name__ == '__main__':
+# 	main()
